2a73ce7ba2e10b946078e98aac8206beebfc5a07
Dhruv <dhruv.sharma994@gmail.com>
2023-09-28 21:21:30 -0400

    initial code setup- added a new page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link"
+
 export default function HomePage() {
     return (
-        <main>
-            <h1> My Blog</h1>
-        </main>
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link href="/about" > About</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1> My Blog</h1>
+            </main>
+        </>
     )
 }
