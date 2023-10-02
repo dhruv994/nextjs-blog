a22da60f2465aa716cea0aae47c07f8b269bfad1
Dhruv <dhruv.sharma994@gmail.com>
2023-10-02 17:42:26 -0400

    added localstorage for default value check

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,15 +1,28 @@
 import { useState } from "react";
 import DarkTheme from "./DarkTheme";
 
+function loadDarkMode() {
+    if(typeof localStorage === 'undefined'){
+        return false;
+    }
+    const value = localStorage.getItem('darkMode');
+    return (value === null) ? false : JSON.parse(value)
+}
 export default function ThemeSwitch() {
 
-    const [darkMode, setDarkMode] = useState(false);
+
+    const [darkMode, setDarkMode] = useState(loadDarkMode);
 
     const text = darkMode ? 'light mode' : 'dark mode'
+    const handleClick = () => {
+        localStorage.setItem('darkMode', JSON.stringify(!darkMode))
+        setDarkMode(!darkMode);
+
+    }
 
     return (
         <>
-            <button onClick={() => setDarkMode(!darkMode)}>{text}</button>
+            <button onClick={() => handleClick()}>{text}</button>
             <style jsx>
                 {`
             button{
